5c36446ba58c65501c4539653df237f70b61de7c
monseroad <monsepino@gmail.com>
2016-11-30 13:02:53 -0300

    lyft a medio terminar :(

---
+++ js/country.js
@@ -0,0 +1,7 @@
+  //Para el menu desplegable de las banderas
+  $(document).ready(function() {
+    $('select').material_select();
+    $('').material_select();
+  });
+
+  
---
+++ js/profile.js
@@ -0,0 +1,5 @@
+//Menu desplegable hacia el lado
+$(document).ready(function() {
+	$(".button-collapse").sideNav();
+});
+
